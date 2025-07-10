e124def81dd64df295d54e0959c676d2617667b0
Jovial307 <pauljovial949@gmail.com>
2025-07-10 11:52:46 +0100

    finished writing code for the resume assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2017;
+        // job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2018;
+        job2._endYear = 2021;
+        // job2.Display();
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Paul Jovial";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
